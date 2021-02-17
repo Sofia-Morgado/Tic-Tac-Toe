c84bdae2e1e55a7305430d510dcb531f7bb6b589
Sofia <sofia.morgado@tecnico.ulisboa.pt>
2021-02-17 18:48:48 +0000

    display tiead message with conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Board from "./components/Board";
 import History from "./components/History";
+import StatusMessage from "./components/StatusMessage";
 import { calculateWinner } from "./helpers";
 
 import "./styles/root.scss";
@@ -18,8 +19,6 @@ const App = () => {
   const current = history[currentMove]; 
 
   const winner = calculateWinner(current.board);
-
-  const message = winner ? `Winner is ${winner}` : `Next player is ${current.isXNext ? 'X' : 'O'}`;
 
   const handleSquareClick = indexClicked => {
       // Check if the board position is already filled, we exit
@@ -53,8 +52,8 @@ const App = () => {
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2>{message}</h2>
-      < Board board={current.board} handleSquareClick={handleSquareClick} />
+      <StatusMessage winner={winner} current={current}/>
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
